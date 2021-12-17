848c6af5ef5617adb7ec1811e6391a48541f9e65
Paulo Rodrigues <paulofilipe_100pt@hotmail.com>
2021-12-17 23:03:23 +0000

    Esqueleto Inicial do projeto

---
+++ lumiere/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row id="nav">
+        <b-col cols="6"> Lumière</b-col>
+        <b-col cols="6">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </b-col>
+        <router-view/>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ lumiere/src/router/index.js
@@ -0,0 +1,77 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import PaginaInicial from '../views/paginaInicial.vue'
+import Filmes from '../views/filmes.vue'
+import Filme from '../views/filme.vue'
+import Jogos from '../views/jogos.vue'
+import Jogo from '../views/jogo.vue'
+import Classificacao from '../views/classificacao.vue'
+import Perfil from '../views/perfil.vue'
+import SobreNos from '../views/sobreNos.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    // Pagina inicial do site
+    path: '/',
+    name: 'paginaInicial',
+    component: PaginaInicial,
+  },
+  {
+    // Pagina com o catologo de filmes
+    path: '/filmes',
+    name: 'filmes',
+    component: Filmes,
+  },
+  {
+    // Pagina com informacoes do filme
+    path: '/filmes/:filmeInfo',
+    name: 'filme',
+    component: Filme,
+  },
+  {
+    // Pagina com o catologo de jogos
+    path: '/jogos',
+    name: 'jogos',
+    component: Jogos,
+  },
+  {
+    // Pagina com o jogo
+    path: '/jogos/:jogo',
+    name: 'jogo',
+    component: Jogo,
+  },
+  {
+    // Pagina com a classificacao dos jogadores
+    path: '/jogos/:classificacao',
+    name: 'classificacao',
+    component: Classificacao,
+  },
+  {
+    // Pagina de perfil
+    path: '/perfil',
+    name: 'perfil',
+    component: Perfil,
+  },
+  {
+    // Pagina que fala sobre o site
+    path: '/sobreNos',
+    name: 'sobreNos',
+    component: SobreNos,
+  },
+  {
+		// Todos as rotas que não sejam as que estão acima definidas é lhes mostrada a página de erro
+		path: '*',
+		name: 'Error',
+		component: Error,
+	},
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
