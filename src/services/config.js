8a45960be44f8c75f97f6a8466ed0ec12814e9eb
Paulo Rodrigues <paulofilipe_100pt@hotmail.com>
2022-06-19 03:21:49 +0100

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
-// const API_URL = 'https://lumiere2122.herokuapp.com/lumiere';
-const API_URL = 'http://127.0.0.1:3000/Lumiere';
+const API_URL = 'https://lumiere2122.herokuapp.com/lumiere';
+// const API_URL = 'http://127.0.0.1:3000/Lumiere';
 export default API_URL;
